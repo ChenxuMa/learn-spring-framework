4be17f670e91bc2ca883b4072343dde305a000f9
ChenxuMa <clarkjones19980430@gmail.com>
2022-12-18 00:53:19 +0100

    loose coupled with Spring

--- src/main/java/com/in28minutes/spring/learnspringframework/game/SuperContraGame.java
+++ src/main/java/com/in28minutes/spring/learnspringframework/game/SuperContraGame.java
@@ -1,16 +1,19 @@
 package com.in28minutes.spring.learnspringframework.game;
 
-public class SuperContraGame {
+import org.springframework.stereotype.Component;
+
+@Component
+public class SuperContraGame implements GamingConsole{
     public void up(){
-        System.out.println("jump");
+        System.out.println("SuperContra jump");
     }
     public void down(){
-        System.out.println("down");
+        System.out.println("SuperContra down");
     }
     public void right(){
-        System.out.println("accelerate");
+        System.out.println("SuperContra accelerate");
     }
     public void left(){
-        System.out.println("stop");
+        System.out.println("SuperContra stop");
     }
 }
